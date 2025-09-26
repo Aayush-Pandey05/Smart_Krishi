56fd26c021c664ff4852436e31bddbe1989e5b98
Aayush-Pandey05 <aayushpandeydsce23@gmail.com>
2025-09-26 16:41:40 +0530

    New changes in backend1

--- backend1/src/index.js
+++ backend1/src/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
-import processRoutes from "./routes/process.route.js";
-import dataRoutes from "./routes/data.route.js";
-import contactRoutes from "./routes/contact.route.js";
-import feedbackRoutes from "./routes/feedback.route.js";
+// import processRoutes from "./routes/process.route.js";
+// import dataRoutes from "./routes/data.route.js";
+// import contactRoutes from "./routes/contact.route.js";
+// import feedbackRoutes from "./routes/feedback.route.js";
 import dotenv from "dotenv";
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -23,10 +23,10 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/processing", processRoutes);
-app.use("/api/data", dataRoutes);
-app.use("/api/contact", contactRoutes);
-app.use("/api/feedback", feedbackRoutes);
+// app.use("/api/processing", processRoutes);
+// app.use("/api/data", dataRoutes);
+// app.use("/api/contact", contactRoutes);
+// app.use("/api/feedback", feedbackRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
